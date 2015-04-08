3aa75ee6ceeae43553d5847208506faa7a47354a
System Administrator <root@Jeremys-MacBook-Air.local>
2015-04-07 22:47:51 -0700

    Re-entering the  notes of prob2a-Winner.py in more organized format.

--- jeremy-python/codeforces/prob2a-Winner.py
+++ jeremy-python/codeforces/prob2a-Winner.py
@@ -1,23 +1,32 @@
-a = input()
+"""
+L14:amount of lines(rounds)of game  
+L15:list to store rounds of game
+L16-17:splits the player and his score
+L18:dictionary to store players and their scores
+L19-20:seperates players and scores into keys and values. Also adds values of 
+L19-20:the same player up.
+L22:reverses the location of the key and values and stores them in a list
+L23:gets one of the keys with the largest value 
+L24:gets the largest value  
+L25:resets d
+L26-27:reruns value addition to determine tiebreakers
+L28-30:checks for the first play to reach target value or above."""
+a = input() 
 b = []
-print b
 for x in range(a):
     b.append(raw_input().split(' '))
 d = {}
 for k,v in b:
     d[k] = int(v) + int(d.get(k, 0))
-print d
 inverse = [(value, key) for key, value in d.items()]
-print inverse
 max_key = max(inverse)[1]
-print max_key
 max_val = d.get(max_key, v)
-print max_val
 d = {}
 for k,v in b:
     d[k] = int(v) + int(d.get(k, 0))
-    if d[k] == max_val:
+    if d[k] >= max_val:
        answer = k
        break
 print answer
+
     
