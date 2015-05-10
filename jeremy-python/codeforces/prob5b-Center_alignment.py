8f75b09299be75560efdf08832900140c59f54d9
System Administrator <root@Jeremys-MacBook-Air.local>
2015-05-09 22:25:46 -0700

    Changes to prob5b-Center_alignment.py 1. Added new comments inside code 2. Added Hash lines seperating blocks of code 3. Changed the variable names from <name>_dictionary to <name>_list

--- jeremy-python/codeforces/prob5b-Center_alignment.py
+++ jeremy-python/codeforces/prob5b-Center_alignment.py
@@ -4,46 +4,60 @@
 # Author: Jeremy Li
 # May 9, 2015
 ##############################################################################
- 
-input_dictionary = []
+input_list = []
 input_scan_val = 0
 max_input_line_length = 0
 align_spaces = 0
 spacing_side1 = 0
 input_print_val = 0
 spacing_side_shifter = 0 
+# defining majority of variables used in code
+##############################################################################
 while True:
     try:                                                     
         line_input = raw_input()
-	input_dictionary.append(line_input)
+	input_list.append(line_input)
     except EOFError:
         break
-max_dictionary_items = len(input_dictionary)-1
-while input_scan_val <= max_dictionary_items: 
-    if max_input_line_length < len(input_dictionary[input_scan_val]):
-        max_input_line_length = len(input_dictionary[input_scan_val]) 
+# taking input from problem
+##############################################################################
+max_list_items = len(input_list)-1
+while input_scan_val <= max_list_items: 
+    if max_input_line_length < len(input_list[input_scan_val]):
+        max_input_line_length = len(input_list[input_scan_val]) 
         input_scan_val = input_scan_val + 1
     else:
         input_scan_val = input_scan_val + 1
+# finds maximum line length
+##############################################################################
 print '*' * (max_input_line_length+2)
-for x in range(len(input_dictionary)):
-   input_line_length  = len(input_dictionary[input_print_val])
+for x in range(len(input_list)):
+   input_line_length  = len(input_list[input_print_val]) 
+   # length of current line
    align_spaces = max_input_line_length - input_line_length
+   # length of total spaces(left and right of line)
    if align_spaces == 0:
-      print ''.join(str(y) for y in ('*',input_dictionary[input_print_val],'*'))
+      # if there are no spaces 
+      print ''.join(str(y) for y in ('*',input_list[input_print_val],'*'))
    elif align_spaces/2 * 2 != align_spaces:
+      # if there are odd spaces
       if spacing_side_shifter == 0:
+          # first put more spaces on left on line
           spacing_side1 = (align_spaces-1)/2
           spacing_side2 = (align_spaces+1)/2
-          print ''.join(str(y) for y in ('*',' ' * spacing_side1,input_dictionary[input_print_val],' ' * spacing_side2,'*'))
+          print ''.join(str(y) for y in ('*',' ' * spacing_side1,input_list[input_print_val],' ' * spacing_side2,'*'))
           spacing_side_shifter = 1
       else:
+          # then put more spaces on right of line
           spacing_side1 = (align_spaces-1)/2
           spacing_side2 = (align_spaces+1)/2
-          print ''.join(str(y) for y in ('*',' ' * spacing_side2,input_dictionary[input_print_val],' ' * spacing_side1,'*'))
+          print ''.join(str(y) for y in ('*',' ' * spacing_side2,input_list[input_print_val],' ' * spacing_side1,'*'))
           spacing_side_shifter = 0
    else:
+       # if there are even spaces
        spacing_side1 = align_spaces/2 
-       print ''.join(str(y) for y in ('*',' ' * spacing_side1,input_dictionary[input_print_val],' ' * spacing_side1,'*'))  
+       print ''.join(str(y) for y in ('*',' ' * spacing_side1,input_list[input_print_val],' ' * spacing_side1,'*'))  
    input_print_val = input_print_val + 1
 print '*' * (max_input_line_length+2)
+# block prints rectangle of astericks, spaces, and centered lines
+##############################################################################
