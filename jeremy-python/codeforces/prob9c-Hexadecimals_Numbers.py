fa197d3b716f92a3bf8df4c5165eaf32f44d2859
System Administrator <root@Jeremys-MacBook-Air.local>
2015-10-25 23:44:19 -0700

    prob9c-Hexadecimals_Numbers.py is fully completed.

--- jeremy-python/codeforces/prob9c-Hexadecimals_Numbers.py
+++ jeremy-python/codeforces/prob9c-Hexadecimals_Numbers.py
@@ -1,11 +1,12 @@
 # Verion 1: uses less memory but more time
+'''
 import array
 number = int(input())
 binaryList = array.array("i", range(number, 0, -1))
 binaryList = [str(i) for i in binaryList]
 count = len([s for s in binaryList if not s.strip('10')])
 print count
-
+'''
 # Version 2: uses more memory but less time
 '''
 number = int(input())
@@ -14,9 +15,13 @@ binaryList = [str(i) for i in binaryList]
 count = len([s for s in binaryList if not s.strip('10')])
 print count
 '''
+# Version 3: Comparison between decimal & binary numbers-success 
+number = input()
+lenNumb = len(str(number))
+count = 0
+for x in range(2**lenNumb - 1):
+    if int(str(bin(x+1))[2:]) <= number:
+        count = count + 1
+print count
 
-# I want to try and combine these lines:
-'''
-binaryList = array.array("i", range(number, 0, -1))
-binaryList = [str(i) for i in binaryList]
-'''
+
