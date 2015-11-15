9b5454c9673375869daa448688f1ca3c3f2b18d1
System Administrator <root@Jeremys-MacBook-Air.local>
2015-11-14 20:24:57 -0800

    Created, finished, and commented prob10a-Power_Consumption_Calculation.py. All tests passed for it.

---
+++ jeremy-python/codeforces/prob10a-Power_Consumption_Calculation.py
@@ -0,0 +1,49 @@
+##################################################
+# http://codeforces.com/problemset/problem/10/A
+# Progam that determines power consumption given:
+# power consumed:on, screensaver, sleep
+# time before:screensaver, sleep
+# work periods
+##################################################
+inputs = raw_input().split(' ')
+shutdownTimes = []
+times = []
+energy = 0
+while 1 == 1:
+    try:
+        times.append(raw_input().split(' '))
+    except EOFError:
+        break
+n = int(inputs[0])
+P1 = int(inputs[1])
+P2 = int(inputs[2])
+P3 = int(inputs[3])
+T1 = int(inputs[4])
+T2 = int(inputs[5])
+##################################################
+# Above takes all input and sorts it
+##################################################
+counter = 0
+for x in range(int(n)):
+    energy = energy + int(P1*(int(times[counter][1])-int(times[counter][0])))
+    counter = counter + 1
+##################################################
+# energy consumed during use of device
+##################################################
+counter = 0
+for x in range(int(n)-1):
+    counter = int(times[x + 1][0])-int(times[x][1])
+    if T1 >= counter:
+        energy = energy + counter*P1
+    else:
+        energy = energy + T1*P1
+        if T2+T1 >= counter:
+            energy = energy + (counter-T1)*P2
+        else:
+            energy = energy + T2*P2
+            energy = energy + (counter - (T1+T2))*P3
+##################################################
+# Energy consumed during screensaver&sleep mode
+##################################################
+print energy
+
