111930eeb1d40e17b55bac3e6de0db407aa2e06d
System Administrator <root@Jeremys-MacBook-Air.local>
2016-04-09 22:11:40 -0700

    Added Khan Acadamy folder and created a working version of a binary search function.

--- jeremy-python/codeforces/UsacoBronzeProb2HaybaleStacking-2012Jan-2.0.py
+++ jeremy-python/codeforces/UsacoBronzeProb2HaybaleStacking-2012Jan-2.0.py
@@ -4,7 +4,8 @@ import time
 start_time = time.time()
 instructions = raw_input().split(' ')
 counter = 0
-point = 0
+apoint = 0
+bpoint = 0
 value = 0
 a = []
 b = []
@@ -18,16 +19,20 @@ a.sort()
 b.sort()
 
 for x in range(int(instructions[0])):
-    value = x+1
-    print value
-    for y in range(a.count(value)):
-        a.remove(value)
-        counter = counter + 1
+    value = x+1    
+    try:
+        while value == a[apoint]:
+            counter += 1
+            apoint += 1
+    except IndexError:
+        True
     bales[x] = counter
-    for y in range(b.count(value)):
-        b.remove(value)
-        counter= counter - 1
-
+    try:
+        while value == b[bpoint]:
+            counter -= 1
+            bpoint += 1
+    except IndexError:
+        True
 bales.sort()
 print bales[(int(instructions[0])-1)/2]
 print("--- %s seconds ---" % (time.time() - start_time))
